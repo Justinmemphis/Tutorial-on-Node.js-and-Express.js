5cfbe4a98dcdacfcdc906767db808c5965379d0d
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-09-28 15:19:35 -0500

    tuesday PM

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-// CommonJS, every file is module (by default)
-// Modules - Encapsulated Code (only share minimum)
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-require('./7-mind-grenade')
-// sayHi('susan')
-// sayHi(names.john)
-// sayHi(names.peter)
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+  if(req.url === '/'){
+    res.end('Welcome to our home page')
+  } else if (req.url === '/about'){
+    res.end('Here is our short history')
+  } else {
+
+    res.end(`
+      <h1>Oops!</h1>
+      <p>We can't seem to find the page you are looking for</p>
+      <a href="/">back home</a>
+    `)
+  }
+})
+
+server.listen(5000)
