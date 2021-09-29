a6e9102b13b35bec6eb3f2b31c086a02a1bf68d9
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-09-28 21:13:27 -0500

    tuesday night

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const http = require('http')
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req,res)=>{
-  if(req.url === '/'){
-    res.end('Welcome to our home page')
-  } else if (req.url === '/about'){
-    res.end('Here is our short history')
-  } else {
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-    res.end(`
-      <h1>Oops!</h1>
-      <p>We can't seem to find the page you are looking for</p>
-      <a href="/">back home</a>
-    `)
-  }
-})
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
 
-server.listen(5000)
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash')
+
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
